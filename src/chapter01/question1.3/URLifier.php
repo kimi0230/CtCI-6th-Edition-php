f8966b030dd730caeaad37439c668a7b423bcd13
kimi <kimi0230@gmail.com>
2019-03-02 13:06:10 +0800

    fix 1.3 bug

--- src/chapter01/question1.3/URLifier.php
+++ src/chapter01/question1.3/URLifier.php
@@ -1,7 +1,9 @@
 <?php
 
-class URLifier {
-    public static function urlify(&$str) {
+class URLifier
+{
+    public static function urlify_org(&$str)
+    {
         // start at the end and swallow all spaces
         $length = strlen($str);
         for ($i = $length - 1; $i >= 0; $i--) {
@@ -24,4 +26,36 @@ class URLifier {
             $i--;
         }
     }
+
+    public static function urlify(&$str)
+    {
+        // print_r(trim($str));
+        $spaceCount = 0;
+        $length = strlen(trim($str));
+
+        // calculate space times, figure out extra char
+        for ($i = 0; $i < $length; $i++) {
+            if ($str[$i] == ' ') {
+                $spaceCount++;
+            }
+        }
+        $index = $length + $spaceCount * 2;
+
+        // fill in string in reverse order
+        for ($i = $length - 1; $i > 0; $i--) {
+            if ($str[$i] === ' ') {
+                $str[--$index] = '0';
+                $str[--$index] = '2';
+                $str[--$index] = '%';
+            } else {
+                $str[--$index] = $str[$i];
+            }
+
+        }
+    }
 }
+
+// my test
+// $test_str = "Mr John Smith    ";
+// URLifier::urlify($test_str);
+// print_r($test_str);
